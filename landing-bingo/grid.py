21d22d5d891cd27b3d1405c65647911f6c9bc36a
Arnaud Muller <arnaud.muller1308@gmail.com>
2023-04-20 22:25:11 +0200

    Grid generation script OK

---
+++ landing-bingo/grid.py
@@ -0,0 +1,82 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+from matplotlib.patches import Circle
+
+img = Image.open("crater.png")
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+fig, ax = plt.subplots(figsize=(img.size[0] / 100, img.size[1] / 100))
+fig.subplots_adjust(left=0, bottom=0, right=1, top=1)
+plt.imshow(img, cmap="gray", interpolation="none")
+
+ax.set_xlim(0, img.size[0])
+ax.set_ylim(img.size[1], 0)
+ax.set_axis_off()
+
+center_x = img.size[0] / 2
+center_y = img.size[1] / 2
+center_coords = (center_x, center_y)
+max_radius = img.size[1] / 2
+
+radii = np.linspace(0, max_radius, 7)
+radii = radii[1:]
+
+center_circle_area = np.pi * radii[0] ** 2
+previous_circle_area = center_circle_area
+
+for ii, radius in enumerate(radii):
+    circle = Circle(
+        center_coords, radius=radius, edgecolor="white", facecolor="none", linewidth=5
+    )
+    ax.add_patch(circle)
+    if ii == 0:
+        ax.text(
+            center_x,
+            center_y,
+            alphabet[ii],
+            color="white",
+            ha="center",
+            va="center",
+            fontsize=50,
+            fontweight="bold",
+        )
+        continue
+    circle_area = np.pi * radius**2
+    segment_area = circle_area - previous_circle_area
+    number_of_zones = np.ceil(segment_area / center_circle_area)
+    zone_angles = np.linspace(0, 2 * np.pi, int(number_of_zones), endpoint=False)
+    if number_of_zones % 2 == 0:
+        zone_angles += np.pi
+    elif ii % 2 == 0:
+        zone_angles += np.pi / 2
+    else:
+        zone_angles -= np.pi / 2
+    text_angles = zone_angles + np.pi / number_of_zones
+    for angle in zone_angles:
+        x_range = [
+            center_x + radius * np.cos(angle),
+            center_x + radii[ii - 1] * np.cos(angle),
+        ]
+        y_range = [
+            center_y + radius * np.sin(angle),
+            center_y + radii[ii - 1] * np.sin(angle),
+        ]
+        ax.plot(x_range, y_range, color="white", linewidth=5)
+    for jj, angle in enumerate(text_angles):
+        x_text = center_x + (radius + radii[ii - 1]) / 2 * np.cos(angle)
+        y_text = center_y + (radius + radii[ii - 1]) / 2 * np.sin(angle)
+        ax.text(
+            x_text,
+            y_text,
+            f"{alphabet[ii]}{jj}",
+            color="white",
+            ha="center",
+            va="center",
+            fontsize=50,
+            fontweight="bold",
+        )
+    previous_circle_area = circle_area
+
+plt.savefig("grid.png")
+plt.show()
